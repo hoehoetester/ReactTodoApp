da814878e3ab73ec8a30f4e672d0ae44891b60f7
Tommy-THINK\Tommy <hoehoetester1@gmail.com>
2018-02-26 22:03:21 +1300

    new branch

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import TodoApp from "./TodoApp";
 const dummyData = [
   { id: 1, todo: "clean room", isCompleted: false },
   { id: 2, todo: "change lightbulb", isCompleted: true },
-  { id: 3, todo: "learn react", isCompleted: false }
+  { id: 3, todo: "learn react", isCompleted: false },
+  { id: 4, todo: "learn react-router", isCompleted: false }
 ];
 
 render(<TodoApp dummyData={dummyData} />, document.getElementById("root"));
